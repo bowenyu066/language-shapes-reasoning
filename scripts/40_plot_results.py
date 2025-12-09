aa0efbad4447ccdb618208fa2bc960e274d17c1c
bowenyu066 <bowenyu@mit.edu>
2025-12-08 21:15:37 -0500

    Adding more plotting results

--- scripts/40_plot_results.py
+++ scripts/40_plot_results.py
@@ -4,32 +4,86 @@ Plot the final results of models.
 """
 import os
 import sys
+import json
 
 # Add parent directory to path for imports
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from src.plotting import plot_multi_bar
+from src.plotting import plot_multi_bar, plot_multi_bar_v2, plot_length_distribution
 
 if __name__ == "__main__":
-    accuracy_en = {
-        "ChatGPT-5.1": 0.7995,
-        "Gemini-2.5-Flash": 0.8182,
-        "DeepSeek-V3.2": 0.8850,
-    }
-    accuracy_zh = {
-        "ChatGPT-5.1": 0.7834,
-        "Gemini-2.5-Flash": 0.8075,
-        "DeepSeek-V3.2": 0.8770,
-    }
-    max_param = 0.9
-    min_param = 0.7
-    plot_multi_bar(
-        accuracy_en,
-        accuracy_zh,
-        max_param,
-        min_param,
-        "Models",
-        "results/mmath/en_vs_zh.pdf",
+    # accuracy_en = {
+    #     "ChatGPT-5.1": 0.7995,
+    #     "Gemini-2.5-Flash": 0.8182,
+    #     "DeepSeek-V3.2": 0.8850,
+    # }
+    # accuracy_zh = {
+    #     "ChatGPT-5.1": 0.7834,
+    #     "Gemini-2.5-Flash": 0.8075,
+    #     "DeepSeek-V3.2": 0.8770,
+    # }
+    # max_param = 0.9
+    # min_param = 0.7
+    # plot_multi_bar(
+    #     accuracy_en,
+    #     accuracy_zh,
+    #     max_param,
+    #     min_param,
+    #     "Models",
+    #     "results/mmath/en_vs_zh.pdf",
+    # )
+    # accuracy_en = {
+    #     "Qwen3-8B": 0.8870,
+    #     "Llama-3.1-8B-Instruct": 0.8029,
+    # }
+    # accuracy_zh = {
+    #     "Qwen3-8B": 0.8939,
+    #     "Llama-3.1-8B-Instruct": 0.5284,
+    # }
+    # accuracy_zh_translate_then_solve = {
+    #     "Qwen3-8B": 0.8984,
+    #     "Llama-3.1-8B-Instruct": 0.4367,
+    # }
+    # max_param = 0.95
+    # min_param = 0.4
+    # plot_multi_bar_v2(
+    #     accuracy_en,
+    #     accuracy_zh,
+    #     accuracy_zh_translate_then_solve,
+    #     max_param,
+    #     min_param,
+    #     "Models",
+    #     "results/gsm8k/en_vs_zh_vs_zh_translate_then_solve.pdf",
+    # )
+    chatgpt_en_path = "results/mmath/summary_chatgpt-5.1_en_direct_8192.json"
+    chatgpt_zh_path = "results/mmath/summary_chatgpt-5.1_zh_direct_8192.json"
+    deepseek_en_path = "results/mmath/summary_deepseek-v3.2_en_direct_8192.json"
+    deepseek_zh_path = "results/mmath/summary_deepseek-v3.2_zh_direct_8192.json"
+    gemini_en_path = "results/mmath/summary_gemini-2.5_en_direct_8192.json"
+    gemini_zh_path = "results/mmath/summary_gemini-2.5_zh_direct_8192.json"
+
+    chatgpt_en_lengths = json.load(open(chatgpt_en_path))["output_lengths"]
+    chatgpt_zh_lengths = json.load(open(chatgpt_zh_path))["output_lengths"]
+    deepseek_en_lengths = json.load(open(deepseek_en_path))["output_lengths"]
+    deepseek_zh_lengths = json.load(open(deepseek_zh_path))["output_lengths"]
+    gemini_en_lengths = json.load(open(gemini_en_path))["output_lengths"]
+    gemini_zh_lengths = json.load(open(gemini_zh_path))["output_lengths"]
+
+    plot_length_distribution(
+        [gemini_en_lengths, gemini_zh_lengths],
+        "results/mmath/length_distribution_gemini.pdf",
+        xname="Output Lengths (Gemini-2.5-Flash)",
+        legend_labels=["Gemini-2.5-Flash (English)", "Gemini-2.5-Flash (Chinese)"],
     )
-    
-    
+    plot_length_distribution(
+        [deepseek_en_lengths, deepseek_zh_lengths],
+        "results/mmath/length_distribution_deepseek.pdf",
+        xname="Output Lengths (DeepSeek-V3.2)",
+        legend_labels=["DeepSeek-V3.2 (English)", "DeepSeek-V3.2 (Chinese)"],
+    )
+    plot_length_distribution(
+        [chatgpt_en_lengths, chatgpt_zh_lengths],
+        "results/mmath/length_distribution_chatgpt.pdf",
+        xname="Output Lengths (ChatGPT-5.1)",
+        legend_labels=["ChatGPT-5.1 (English)", "ChatGPT-5.1 (Chinese)"],
+    )
--- src/plotting.py
+++ src/plotting.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from matplotlib.patches import Rectangle
 from matplotlib.legend_handler import HandlerBase
 from matplotlib.colors import to_rgba
+from scipy.stats import gaussian_kde
 
 class HandlerMultiSquare(HandlerBase):
     def create_artists(self, legend, orig_handle,
@@ -22,6 +23,7 @@ class HandlerMultiSquare(HandlerBase):
                           edgecolor='none')
             artists.append(r)
         return artists
+
 
 def _rescale(
     data: np.ndarray,
@@ -98,3 +100,142 @@ def plot_multi_bar(
         plt.title(title)
     plt.tight_layout()
     plt.savefig(save_path)
+
+
+def plot_multi_bar_v2(
+    params_en: Dict[str, float],
+    params_zh: Dict[str, float],
+    params_zh_translate_then_solve: Dict[str, float],
+    max_param: float,
+    min_param: float,
+    xname: str,
+    save_path: Path,
+    title: str = "",
+    legend: bool = True,
+) -> None:
+    """
+    Plot the bar chart of parameters for English, Chinese, and Chinese after translation and then solve.
+    
+    Args:
+        params_en (dict[str, float]): The parameter values for English model.
+        params_zh (dict[str, float]): The parameter values for Chinese model.
+        params_zh_translate_then_solve (dict[str, float]): The parameter values for Chinese model after translation and then solve.
+        max_param (float): The maximum value for normalization.
+        min_param (float): The minimum value for normalization.
+        xname (str): The name of the x-axis.
+        save_path (Path): The path to save the plot.
+        title (str): The title of the plot.
+    """
+    plt.rcParams["font.family"] = "arial"  
+    plt.rcParams["font.size"] = 18
+    # plt.rcParams['mathtext.fontset'] = 'custom'
+    # plt.rcParams['mathtext.rm'] = 'arial'
+    # plt.rcParams['mathtext.it'] = 'arial:italic'
+    # plt.rcParams['mathtext.bf'] = 'arial:bold'
+    
+    width = 0.27
+    colors = ("#96ced3", "#e9c54e", "#e64b35")
+    # colors_alpha = tuple(to_rgba(c, alpha=0.3) for c in colors)
+    # colors_alpha_2 = tuple(to_rgba(c, alpha=0.6) for c in colors)
+    x = np.arange(len(params_en))
+    
+    fig, ax = plt.subplots(figsize=(14, 6))
+    # English results
+    keys, values_en = zip(*params_en.items())
+    norm_values_en = _rescale(np.array(values_en), min_param, max_param)
+    rects_en = ax.bar(x - width / 2, norm_values_en, width, label="English", color=colors[0])
+    values_en = [f"{v:.4f}" for v in values_en]
+    ax.bar_label(rects_en, labels=values_en, padding=3, fmt="%.4f")
+    # Chinese results
+    keys, values_zh = zip(*params_zh.items())
+    norm_values_zh = _rescale(np.array(values_zh), min_param, max_param)
+    rects_zh = ax.bar(x + width / 2, norm_values_zh, width, label="Chinese", color=colors[1])
+    values_zh = [f"{v:.4f}" for v in values_zh]
+    ax.bar_label(rects_zh, labels=values_zh, padding=3, fmt="%.4f")
+    # Chinese translate then solve results
+    keys, values_zh_translate_then_solve = zip(*params_zh_translate_then_solve.items())
+    norm_values_zh_translate_then_solve = _rescale(np.array(values_zh_translate_then_solve), min_param, max_param)
+    rects_zh_translate_then_solve = ax.bar(x + width * 3 / 2, norm_values_zh_translate_then_solve, width, label="Chinese→English", color=colors[2])
+    values_zh_translate_then_solve = [f"{v:.4f}" for v in values_zh_translate_then_solve]
+    ax.bar_label(rects_zh_translate_then_solve, labels=values_zh_translate_then_solve, padding=3, fmt="%.4f")
+    
+    ax.set_xticks(x + width / 2, keys)
+    ax.set_yticks([])
+    ax.set_xlabel(xname)
+    ax.set_ylim(0.0, 1.0)
+    if legend:
+        ax.legend(loc='upper right', bbox_to_anchor=(1.4, 1))
+    if title:
+        plt.title(title)
+    plt.tight_layout()
+    plt.savefig(save_path)
+
+
+def plot_length_distribution(
+    token_lengths: List[List[int]],
+    save_path: Path,
+    xname: str,
+    title: str = "",
+    legend: bool = True,
+    legend_labels: List[str] = None,
+    smooth: bool = True,
+    plot_avg: bool = True,
+) -> None:
+    plt.rcParams["font.family"] = "arial"  
+    plt.rcParams["font.size"] = 18
+    # plt.rcParams['mathtext.fontset'] = 'custom'
+    # plt.rcParams['mathtext.rm'] = 'arial'
+    # plt.rcParams['mathtext.it'] = 'arial:italic'
+    # plt.rcParams['mathtext.bf'] = 'arial:bold'
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    colors = ["#80ae9a", "#f6d6c2"]
+    outlines_colors = ["#568b87", "#d47264"]
+
+    for i, token_length in enumerate(token_lengths):
+        token_length = np.array(
+            [L for L in token_length if 0 < L < 15000]
+        )
+
+        # Histogram (set density=True so KDE is on the same scale)
+        ax.hist(
+            token_length,
+            bins=50,
+            alpha=0.5,
+            density=True,
+            color=colors[i % len(colors)],
+            label=legend_labels[i] if legend_labels is not None else None,
+        )
+
+        if smooth and len(token_length) > 1:
+            kde = gaussian_kde(token_length, bw_method=0.05)
+            xs = np.linspace(token_length.min(), token_length.max(), 500)
+            ys = kde(xs)
+
+            # Smooth outline
+            ax.plot(xs, ys, lw=5, color=outlines_colors[i % len(outlines_colors)], linestyle="-")
+
+            # “Enclosed outline” – fill under the curve (optional)
+            # ax.fill_between(xs, ys, alpha=0.15, color=colors[i % len(colors)])
+
+    if plot_avg:
+        y_max = ax.get_ylim()[1]
+        for i, token_length in enumerate(token_lengths):
+            avg = np.mean(token_length)
+            ax.vlines(avg, 0, y_max, color=outlines_colors[i % len(outlines_colors)], linestyle="--", lw=5)
+                
+
+    ax.set_xlabel("Output Lengths" if xname is None else xname)
+    ax.set_yticks([])
+
+    if legend:
+        ax.legend(loc='upper right')
+
+    if title:
+        ax.set_title(title)
+
+    plt.tight_layout()
+    plt.savefig(save_path)
+    plt.close(fig)
+    
